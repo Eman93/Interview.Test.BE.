7196a750e6ee2378b628ae1ec248d9b434a480cd
Eman93 <eman.m.elsheikh@gmail.com>
2022-11-25 12:27:35 -0500

    cleaning the code in graduation tracker

--- GraduationTracker/GraduationTracker/GraduationTracker.cs
+++ GraduationTracker/GraduationTracker/GraduationTracker.cs
@@ -10,35 +10,68 @@ namespace GraduationTracker
     {   
         public Tuple<bool, STANDING>  HasGraduated(Diploma diploma, Student student)
         {
-            var credits = 0;
-            var average = 0;
-        
-            for(int i = 0; i < diploma.Requirements.Length; i++)
-            {
-                for(int j = 0; j < student.Courses.Length; j++)
-                {
-                    var requirement = Repository.GetRequirement(diploma.Requirements[i]);
+            int studentCredits = 0, studentTotalMarks = 0, numberOfCoursesTaken=0 ;
 
-                    for (int k = 0; k < requirement.Courses.Length; k++)
+            // get diploma requirements 
+            var requirements = Repository.GetRequirements().Where(req => diploma.Requirements.Any(reqId => reqId == req.Id));
+
+            foreach (var req in requirements)
+            {
+                /*
+                 I think "Requirement" object should have one to one relationship with "Course"
+                 not one to many
+
+                 so the code would have been as follow,and wouldn't use another foreach
+
+                  int courseId = req.Courses.SingleOrDefault();           
+
+                 */
+
+                //check student mark for courses in the requirment
+
+                bool hasPassedAllCourses = true;
+                foreach (var courseId in req.Courses)
+                {
+                    var studentCourse = student.Courses.Where(course => course.Id == courseId).SingleOrDefault();
+
+                    if (studentCourse!=null && studentCourse.Mark >= req.MinimumMark)
                     {
-                        if (requirement.Courses[k] == student.Courses[j].Id)
-                        {
-                            average += student.Courses[j].Mark;
-                            if (student.Courses[j].Mark > requirement.MinimumMark)
-                            {
-                                credits += requirement.Credits;
-                            }
-                        }
+                        studentTotalMarks += studentCourse.Mark;
+                        numberOfCoursesTaken++;
+
                     }
+                    else { hasPassedAllCourses = false; }
                 }
+
+                studentCredits += hasPassedAllCourses ? req.Credits : 0;
+
             }
 
-            average = average / student.Courses.Length;
+
+            // if student criedts are not enough, no need to calculate the standing
+            if(studentCredits < diploma.Credits)
+            {
+                return new Tuple<bool, STANDING>(false, STANDING.None);
+            }
+
+            /*
+             * I am not sure how we should calculate the standing or the gpa in general and
+             * if we need the course full mark or no (it is not given).
+             * So I will leave the calculation logic as it is for now with changes in the code style itself
+             * 
+             */
+
+
+            bool hasGraduated = true;
+            int average = studentTotalMarks / numberOfCoursesTaken;
 
             var standing = STANDING.None;
 
             if (average < 50)
+            {
                 standing = STANDING.Remedial;
+                hasGraduated = false;
+            }
             else if (average < 80)
                 standing = STANDING.Average;
             else if (average < 95)
@@ -46,20 +79,8 @@ namespace GraduationTracker
             else
                 standing = STANDING.SumaCumLaude;
 
-            switch (standing)
-            {
-                case STANDING.Remedial:
-                    return new Tuple<bool, STANDING>(false, standing);
-                case STANDING.Average:
-                    return new Tuple<bool, STANDING>(true, standing);
-                case STANDING.SumaCumLaude:
-                    return new Tuple<bool, STANDING>(true, standing);
-                case STANDING.MagnaCumLaude:
-                    return new Tuple<bool, STANDING>(true, standing);
+            return new Tuple<bool, STANDING>(hasGraduated, standing);
 
-                default:
-                    return new Tuple<bool, STANDING>(false, standing);
-            } 
         }
     }
 }
